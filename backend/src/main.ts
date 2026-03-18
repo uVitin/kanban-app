fb66d8f3943d051fc3fe8b8da40081a15be8291d
AlbertVitor <albert.vitor@icloud.com>
2026-03-18 17:36:08 -0300

    fix(backend): configurando CORS para aceitar dominio da Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://kanban-jcnwq7ap5-albert-vitors-projects.vercel.app',
+      /\.vercel\.app$/,
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
